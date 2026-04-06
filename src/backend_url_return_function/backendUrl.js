563dfb5beff5f7dd80cddf1d031160324b4721c2
Goutam Mandal <goutammandal@Goutams-MacBook-Air.local>
2026-04-06 13:09:05 +0530

    updateee bak url

--- src/backend_url_return_function/backendUrl.js
+++ src/backend_url_return_function/backendUrl.js
@@ -1,6 +1,6 @@
 
-const backend_Url = 'http://localhost:3000';
-//const backend_Url = 'https://uemfood-backend.onrender.com';
+//const backend_Url = 'http://localhost:3000';
+const backend_Url = 'https://uemfood-backend.onrender.com';
 
 export default backend_Url;
 
